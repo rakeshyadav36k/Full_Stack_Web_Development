aeec9ef9d0f33a6b411948664cad1d72d59db5bb
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-01-16 22:09:10 +0530

    set up the chat container

--- Chat App/public/src/pages/Chat.jsx
+++ Chat App/public/src/pages/Chat.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
+import { allUserRoute } from '../utils/APIRoutes';
 
 function Chat() {
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const navigate = useNavigate();
+
+  // in both useEffect we have to add cleanup function
+  useEffect(async() => {
+    if(!localStorage.getItem("chat-app-user")){
+        navigate("/login");
+    } else{
+      setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")))
+    }
+  }, [])
+
+  useEffect(async()=>{
+    if(currentUser){
+      if(currentUser.isAvatarImageSet){
+        const data = await axios.get(`${allUserRoute}/${currentUser._id}`)
+        setContacts(data.data)
+      } else{
+        navigate("/setAvatar")
+      }
+    }
+  }, [currentUser])
+
   return (
-    <div>
-      Chat
-    </div>
+    <Container>
+      <div className="container">
+
+      </div>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container{
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%
+    @media screen and (min-width: 720px) and (max-width: 1080px){
+      grid-template-columns: 35% 65%;
+    }
+  }
+`
 
 export default Chat
--- Chat App/server/routes/userRoutes.js
+++ Chat App/server/routes/userRoutes.js
@@ -1,4 +1,4 @@
-const { register, login, setAvatar } = require("../controllers/usersControllers");
+const { register, login, setAvatar, getAllUsers } = require("../controllers/usersControllers");
 
 
 const router = require("express").Router();
@@ -6,5 +6,6 @@ const router = require("express").Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/setAvatar/:id", setAvatar);
+router.get("/allusers/:id", getAllUsers)
 
 module.exports = router;
