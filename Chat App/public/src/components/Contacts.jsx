ce70844eab85e648cbe91dd083e7b262af460982
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-02-11 23:46:29 +0530

    Update Contacts.jsx

--- Chat App/public/src/components/Contacts.jsx
+++ Chat App/public/src/components/Contacts.jsx
@@ -7,7 +7,6 @@ export default function Contacts({contacts, currentUser}) {
     const [currentUserImage, setCurrentUserImage] = useState(undefined)
     const [currentSelected, setCurrentSelected] = useState(undefined)
     useEffect(()=>{
-      // console.log(contacts);
         if(currentUser){
           setCurrentUserImage(currentUser.avatarImage)
           setCurrentUserName(currentUser.username)
