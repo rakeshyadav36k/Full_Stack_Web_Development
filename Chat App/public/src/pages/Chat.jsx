bf0df56027c4f74f7261250c23b0ef572df3bc2b
Rakesh Yadav <rakeshyadav36k@gmail.com>
2025-02-14 08:14:33 +0530

    Update Chat.jsx

--- Chat App/public/src/pages/Chat.jsx
+++ Chat App/public/src/pages/Chat.jsx
@@ -4,10 +4,14 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
 import { allUserRoute } from '../utils/APIRoutes';
 import Contacts from '../components/Contacts';
+import Welcome from '../components/Welcome';
+import ChatContainer from '../components/ChatContainer';
 
 function Chat() {
   const [contacts, setContacts] = useState([]);
   const [currentUser, setCurrentUser] = useState(undefined);
+  const [currentChat, setCurrentChat] = useState(undefined);
+  const [isLoaded, setIsLoaded] = useState(false);
   const navigate = useNavigate();
 
   // in both useEffect we have to add cleanup function
@@ -17,6 +21,7 @@ function Chat() {
           navigate("/login");
       } else{
         setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")))
+        setIsLoaded(true);
       }
     }
     fn()  // this is basically a  cleanup function because in useEffect, we can't make the callback function as async
@@ -36,10 +41,22 @@ function Chat() {
     fetchData()
   }, [currentUser])
 
+  const handleChatChange = (chat) =>{
+    setCurrentChat(chat)
+  }
   return (
     <Container>
       <div className="container">
-        <Contacts contacts = {contacts} currentUser = {currentUser} />
+        <Contacts 
+          contacts = {contacts} 
+          currentUser = {currentUser} 
+          changeChat = {handleChatChange} 
+        />
+        {
+          isLoaded && currentChat === undefined ?
+          <Welcome currentUser = {currentUser} /> :
+          <ChatContainer currentChat = {currentChat} currentUser = {currentUser}/>
+        }
       </div>
     </Container>
   )
